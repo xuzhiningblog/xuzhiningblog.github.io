f95ead83fb18a6bc304b1cefe9ad8544e9113190
lucky <1532836176@qq.com>
2018-03-07 13:46:46 +0800

    Site updated: 2018-03-07 13:46:43

--- myaxure/files/图片组件/data.js
+++
@@ -1,7 +0,0 @@
-$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n],o,_(p,q,r,s,t,u,v,_(),w,_(x,y,z,A,B,_(C,D,E,F),G,null,H,I,J,I,K,L,M,null,N,O,P,Q,R,S,T,O),U,_(),V,_(),W,_(X,[])),Y,_(),Z,_());}; 
-var b="url",c="图片组件.html",d="generationDate",e=new Date(1516083107173.7),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="Menu",o="page",p="packageId",q="58b1da812484413dad5c28e46240ac26",r="type",s="Axure:Page",t="name",u="图片组件",v="notes",w="style",x="baseStyle",y="627587b6038d43cca051c114ac41ad32",z="pageAlignment",A="center",B="fill",C="fillType",D="solid",E="color",F=0xFFF6F7F9,G="image",H="imageHorizontalAlignment",I="near",J="imageVerticalAlignment",K="imageRepeat",L="auto",M="favicon",N="sketchFactor",O="0",P="colorStyle",Q="appliedColor",R="fontName",S="Applied Font",T="borderWidth",U="adaptiveStyles",V="interactionMap",W="diagram",X="objects",Y="masters",Z="objectPaths";
-return _creator();
-})());
